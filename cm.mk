13d70a686cc0425b1c19ee3c08c84f6ea6139393
Yahya Wessam <yahyawessam2002@gmail.com>
2016-03-31 10:12:30 +0300

    Phoenix-ify

--- cm.mk
+++ cm.mk
@@ -6,7 +6,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-# Inherit some common JDC stuff.
-$(call inherit-product, vendor/jdc/config/common.mk)
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common.mk)
 
-PRODUCT_NAME := cm_jflte
+PRODUCT_NAME := phoenix_jflte
