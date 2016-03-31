e577e626a038a06e4fd9185eaa5963d700735577
Yahya Wessam <yahyawessam2002@gmail.com>
2016-03-31 10:00:46 +0300

    Turbo-ify

--- cm.mk
+++ cm.mk
@@ -4,9 +4,9 @@ $(call inherit-product, device/samsung/jflte/full_jflte.mk)
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/turbo/config/common_full_phone.mk)
 
-# Inherit some common JDC stuff.
-$(call inherit-product, vendor/jdc/config/common.mk)
+# Inherit some common Turbo stuff.
+$(call inherit-product, vendor/turbo/config/common.mk)
 
-PRODUCT_NAME := cm_jflte
+PRODUCT_NAME := turbo_jflte
