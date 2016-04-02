64c03dbd87a361835318c234dc4fa937248ac600
Yahya Wessam <yahyawessam2002@gmail.com>
2016-04-02 07:51:45 +0300

    fix breakfast error and turbo-ify

--- turbo.mk
+++ turbo.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/samsung/jflte/full_jflte.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/turbo/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/turbo/config/common_full_phone.mk)
